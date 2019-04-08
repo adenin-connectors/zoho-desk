51d782e439b9445c7137de627ed827c484955b76
snt_danilo_markovic <dmarkovic@smartnet.rs>
2019-04-08 09:54:24 +0200

    Added activity 'open-tickets-by-priority.js'.

---
+++ activities/open-tickets-by-priority.js
@@ -0,0 +1,61 @@
+'use strict';
+const api = require('./common/api');
+
+module.exports = async (activity) => {
+  try {
+    const response = await api.getTickets();
+
+    if (Activity.isErrorResponse(response, [200, 204])) return;
+
+    activity.Response.Data = mapResponseToChartData(response);
+  } catch (error) {
+    Activity.handleError(error);
+  }
+};
+//** maps response data to data format usable by chart */
+function mapResponseToChartData(response) {
+  let tickets = response.body.data;
+  let priorities = [];
+  let datasets = [];
+  let data = [];
+
+  for (let i = 0; i < tickets.length; i++) {
+    let priority = tickets[i].priority ? tickets[i].priority : "No Priority";
+    if (!priorities.includes(priority)) {
+      priorities.push(priority);
+    }
+  }
+
+  for (let x = 0; x < priorities.length; x++) {
+    let counter = 0;
+    for (let y = 0; y < tickets.length; y++) {
+      let status = tickets[y].priority ? tickets[y].priority : "No Priority";
+      if (priorities[x] == status) {
+        counter++;
+      }
+    }
+    data.push(counter);
+  }
+  datasets.push({ label: 'Number Of Tickets', data });
+
+  let chartData = {
+    chart: {
+      configuration: {
+        data: {},
+        options: {
+          title: {
+            display: true,
+            text: 'Ticket Metrics By Priority'
+          }
+        }
+      },
+      template: 'bar',
+      palette: 'office.Office6'
+    },
+    _settings: {}
+  };
+  chartData.chart.configuration.data.labels = priorities;
+  chartData.chart.configuration.data.datasets = datasets;
+
+  return chartData;
+}
