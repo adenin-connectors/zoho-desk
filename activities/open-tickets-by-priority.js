89ab4096a8955bc688480c16cf4fad305ed5e72a
olliejm <olliejm@protonmail.com>
2019-11-26 12:14:18 +0000

    adjust open-tickets-by-priority

--- activities/open-tickets-by-priority.js
+++ activities/open-tickets-by-priority.js
@@ -1,61 +1,98 @@
 'use strict';
+
 const api = require('./common/api');
 
 module.exports = async (activity) => {
   try {
     api.initialize(activity);
-    const response = await api.getTickets();
+
+    await api.initOrgId();
+
+    const limit = 99;
+
+    let offset = 0;
+    let url = `/tickets?include=contacts,assignee,departments,team,isRead&status=open&from=${offset}&limit=${limit}&sortBy=-createdTime`;
+
+    let response = await api(url);
 
     if ($.isErrorResponse(activity, response, [200, 204])) return;
 
-    activity.Response.Data = mapResponseToChartData(response);
+    const tickets = [];
+
+    tickets.push(...response.body.data);
+
+    let hasMore = false;
+
+    if (response.body.data.length === limit) hasMore = true;
+
+    while (hasMore) {
+      offset += limit;
+      url = `/tickets?include=contacts,assignee,departments,team,isRead&status=open&from=${offset}&limit=${limit}&sortBy=-createdTime`;
+
+      response = await api(url);
+
+      if ($.isErrorResponse(activity, response)) return;
+
+      tickets.push(...response.body.data);
+
+      if (response.body.data.length !== limit) hasMore = false;
+    }
+
+    const chartDefinition = createChartDefinition(tickets);
+
+    activity.Response.Data.chart = chartDefinition;
+    activity.Response.Data.title = T(activity, 'Open Tickets by Priority');
+    activity.Response.Data.link = `https://desk.zoho.com/support/${activity.Context.connector.custom1}/ShowHomePage.do#Cases`;
+    activity.Response.Data.linkLabel = T(activity, 'All Tickets');
   } catch (error) {
     $.handleError(activity, error);
   }
 };
+
 //** maps response data to data format usable by chart */
-function mapResponseToChartData(response) {
-  let tickets = response.body.data;
-  let priorities = [];
-  let datasets = [];
-  let data = [];
+function createChartDefinition(tickets) {
+  const priorities = [];
+  const datasets = [];
+  const data = [];
 
   for (let i = 0; i < tickets.length; i++) {
-    let priority = tickets[i].priority ? tickets[i].priority : "No Priority";
-    if (!priorities.includes(priority)) {
-      priorities.push(priority);
-    }
+    const priority = tickets[i].priority ? tickets[i].priority : 'No Priority';
+
+    if (!priorities.includes(priority)) priorities.push(priority);
   }
 
   for (let x = 0; x < priorities.length; x++) {
     let counter = 0;
+
     for (let y = 0; y < tickets.length; y++) {
-      let status = tickets[y].priority ? tickets[y].priority : "No Priority";
-      if (priorities[x] == status) {
-        counter++;
-      }
+      const status = tickets[y].priority ? tickets[y].priority : 'No Priority';
+
+      if (priorities[x] === status) counter++;
     }
+
     data.push(counter);
   }
-  datasets.push({ label: 'Number Of Tickets', data });
 
-  let chartData = {
-    chart: {
-      configuration: {
-        data: {},
-        options: {
-          title: {
-            display: true,
-            text: 'Ticket Metrics By Priority'
-          }
-        }
+  datasets.push({
+    label: 'Number Of Tickets',
+    data
+  });
+
+  const chart = {
+    configuration: {
+      data: {
+        labels: priorities,
+        datasets: datasets
       },
-      template: 'pie',
+      options: {
+        title: {
+          display: true,
+          text: 'Open Tickets by Priority'
+        }
+      }
     },
-    _settings: {}
+    template: 'pie-labels'
   };
-  chartData.chart.configuration.data.labels = priorities;
-  chartData.chart.configuration.data.datasets = datasets;
 
-  return chartData;
+  return chart;
 }
